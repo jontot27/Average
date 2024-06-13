4ae04956ebdc49840ffce3e07757c4f2ef93da11
jontot27 <j.a.ballesterosjr@gmail.com>
2024-06-14 03:18:41 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let fil = 97;
+let sci = 98;
+let eng = 95;
+let mat = 99;
+let his = 100;
+let ave = 5;
+
+alert(`English = ${eng}
+Science = ${sci}
+Filipino = ${fil}
+Math = ${mat}
+History = ${his}
+Ave: ${(fil + sci + eng + mat + his) / ave}`);
